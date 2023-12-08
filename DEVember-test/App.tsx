788bf24c67c71b9023fbe470a1d920d8a66991a9
José Lopes <946393361bill@gmail.com>
2023-12-08 00:28:06 +0000

    building boxes

--- DEVember-test/App.tsx
+++ DEVember-test/App.tsx
@@ -2,10 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Hora de React !</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.box}>
+        <Text style={styles.Text}>1</Text>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={styles.Text}>2</Text>
+      </View>
+
+      <View style={styles.box}>
+        <Text style={styles.Text}>3</Text>
+      </View>
+     
+      
     </View>
   );
 }
@@ -16,5 +29,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
+    gap: 10,
   },
+
+
+  box: {
+    backgroundColor: '#e8d3c2',
+    width: 300,
+    height: 300,
+
+    borderWidth: StyleSheet.hairlineWidth, // deixar a borda fina
+    borderColor: '#9b4521',
+    borderRadius: 20,
+    
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  Text: {
+    color: '#9b4521',
+    fontSize: 58
+  }
+
 });
