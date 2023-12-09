828079abaeae4398860a1b7983204fcdd1c831c8
José Lopes <946393361bill@gmail.com>
2023-12-09 16:18:04 +0000

    fetchind array data at FlatList

--- DEVember-test/App.tsx
+++ DEVember-test/App.tsx
@@ -1,24 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const days = [1, 7, 5,4, 2];
 
 export default function App() {
-  
+
   return (
     <View style={styles.container}>
-      
-      <View style={styles.box}>
-        <Text style={styles.Text}>1</Text>
-      </View>
 
-      <View style={styles.box}>
-        <Text style={styles.Text}>2</Text>
-      </View>
+      <FlatList
+        data={days}
+        renderItem={({ item }) => (
+          <View style={styles.box} >
+            <Text style={styles.Text}>{item}</Text>
+          </View>
+        )}
+      />
 
-      <View style={styles.box}>
-        <Text style={styles.Text}>3</Text>
-      </View>
-     
-      
+      { /*days.map((day) => (
+        <View style={styles.box} key={day}>
+          <Text style={styles.Text}>{day}</Text>
+        </View>
+      ))*/}
+
+
+
+      <StatusBar style='auto' />
+
+
     </View>
   );
 }
@@ -42,7 +51,7 @@ const styles = StyleSheet.create({
     borderWidth: StyleSheet.hairlineWidth, // deixar a borda fina
     borderColor: '#9b4521',
     borderRadius: 20,
-    
+
     justifyContent: 'center',
     alignItems: 'center'
   },
