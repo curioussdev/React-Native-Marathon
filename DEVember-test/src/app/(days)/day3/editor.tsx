4357ad546aeb185ba424765a0b7eb5205e544bc6
José Lopes <946393361bill@gmail.com>
2024-01-23 23:03:05 +0000

    first markdown

--- DEVember-test/src/app/(days)/day3/editor.tsx
+++ DEVember-test/src/app/(days)/day3/editor.tsx
@@ -1,10 +1,20 @@
 import { View, Text} from 'react-native';
 import React from 'react';
 
+import Markdown from 'react-native-markdown-display';
+
+const copy = `# h1 Heading 8-)
+
+**This is some bold text!**
+
+This is normal text
+
+`;
+
 const EditorScreen = () => {
     return (
         <View>
-            <Text>EditorScreen</Text>
+            <Markdown>{copy}</Markdown>
         </View>
     );
 };
