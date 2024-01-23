92601ff2ebca0d391265dc59857fc0ae8c785e6e
José Lopes <946393361bill@gmail.com>
2024-01-23 22:38:47 +0000

    day 3 begin

--- DEVember-test/src/app/(days)/day3/index.tsx
+++ DEVember-test/src/app/(days)/day3/index.tsx
@@ -5,7 +5,7 @@ import { Link, Stack } from 'expo-router'
 const Day2 = () => {
     return (
         <View>
-            <Stack.Screen options={{ title: 'Day 2: Onboarding Screen' }} />
+            <Stack.Screen options={{ title: 'Day 3: Markdown' }} />
             <Text>Day 2</Text>
 
             <Link href="/day2/onboarding" asChild >
