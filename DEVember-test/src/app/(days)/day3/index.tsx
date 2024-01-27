c4a1ad76492a8aaefe890c1b8157f532b858a07c
José Lopes <946393361bill@gmail.com>
2024-01-27 14:27:44 +0000

    Integrate MRKDW with RN

--- DEVember-test/src/app/(days)/day3/index.tsx
+++ DEVember-test/src/app/(days)/day3/index.tsx
@@ -1,18 +1,27 @@
 import { View, Text, Button } from 'react-native';
 import React from 'react';
-import { Link, Stack } from 'expo-router'
+import { Link, Stack } from 'expo-router';
+import { MarkDownDisplay } from '@/components/day3/MarkDownDisplay';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const description = `
+# Markdown
+
+Integrate Markdown Content in **React Native**
+`;
 
 const Day3 = () => {
     return (
-        <View>
+        <SafeAreaView edges={['bottom']}  style={{ flex: 1}}>
             <Stack.Screen options={{ title: 'Day 3: Markdown' }} />
-            <Text>Day 3</Text>
+ 
+            <MarkDownDisplay>{description}</MarkDownDisplay>
 
             <Link href="/day3/editor" asChild >
                 <Button title="Go To Editor" />
             </Link>
 
-        </View>
+        </SafeAreaView>
     )
 }
 
