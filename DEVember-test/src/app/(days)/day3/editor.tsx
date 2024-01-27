50b3af3662d25972fb76a9669b532c16dd3d75a1
José Lopes <946393361bill@gmail.com>
2024-01-27 15:25:45 +0000

    styling the edit & preview button

--- DEVember-test/src/app/(days)/day3/editor.tsx
+++ DEVember-test/src/app/(days)/day3/editor.tsx
@@ -14,7 +14,24 @@ const EditorScreen = () => {
     const [content, setContent ] = useState(template)
     return (
         <View style={styles.page}>
-            <TextInput value={content} multiline />
+            <View style={styles.tabsContainer}>
+
+                <View style={styles.tab}>
+                    <Text style={styles.tabText}>Edit</Text>
+                </View>
+                
+                <View style={styles.tab}>
+                    <Text style={styles.tabText}>Preview</Text>
+                </View>
+            </View>
+
+            <TextInput 
+                value={content} 
+                multiline 
+                numberOfLines={50}
+                style={styles.input}
+                onChangeText={setContent}
+                />
 
             {/*<MarkDownDisplay>{content}</MarkDownDisplay>*/}
         </View>
@@ -24,11 +41,35 @@ const EditorScreen = () => {
 
 const styles = StyleSheet.create({
     page: {
-        backgroundColor: 'white',
+        backgroundColor: 'whitesmoke',
         flex: 1,
         padding: 10,
-        paddingBottom: 10,
-    }
+        
+    },
+
+    input: {
+        backgroundColor: 'white',
+        flex: 1,
+        padding: 20,
+        borderRadius: 10,
+        fontSize: 16
+    },
+    tabsContainer: {
+        flexDirection: 'row',
+        gap: 10,
+        marginVertical: 10,
+    },
+    tab: {
+        flex: 1,
+        padding: 10,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    tabText: {
+        fontFamily: 'InterBold',
+    },
 });
 
 export default EditorScreen;
