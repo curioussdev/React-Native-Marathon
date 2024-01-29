8b4f1238bec7d32a0abc12ee4eb1a3e229d3b72d
José Lopes <946393361bill@gmail.com>
2024-01-29 22:45:23 +0000

    testing

---
+++ DEVember-test/src/app/(days)/day4/index.tsx
@@ -0,0 +1,28 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { Link, Stack } from 'expo-router';
+import { MarkDownDisplay } from '@/components/day3/MarkDownDisplay';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const description = `
+# Markdown
+
+
+`;
+
+const Day3 = () => {
+    return (
+        <SafeAreaView edges={['bottom']}  style={{ flex: 1}}>
+            <Stack.Screen options={{ title: 'Day 3: Markdown' }} />
+ 
+            <MarkDownDisplay>{description}</MarkDownDisplay>
+
+            <Link href="/day3/editor" asChild >
+                <Button title="Go To Editor" />
+            </Link>
+
+        </SafeAreaView>
+    )
+}
+
+export default Day3;
