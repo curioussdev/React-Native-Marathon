fc44643d01caa202ca4baf7d0c472ad829881736
José Lopes <946393361bill@gmail.com>
2024-02-04 21:53:53 +0000

    changing the title

--- DEVember-test/src/app/(days)/day4/animation.tsx
+++ DEVember-test/src/app/(days)/day4/animation.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const AnimationScreen = () => {
     return (
         <View>
-            <Text>AnimationScreen</Text>
+            <Text>Netflix Animation Screen Test</Text>
         </View>
     );
 };
